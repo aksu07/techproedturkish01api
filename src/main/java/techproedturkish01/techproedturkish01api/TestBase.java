aca36fcfe0942536ea0c9de305a8027202f92368
apple <apple@apples-imac-6.attlocal.net>
2020-08-21 16:28:27 -0400

    Get ended

--- src/main/java/techproedturkish01/techproedturkish01api/TestBase.java
+++ src/main/java/techproedturkish01/techproedturkish01api/TestBase.java
@@ -8,11 +8,27 @@ import io.restassured.specification.RequestSpecification;
 public class TestBase {
 	
 	protected RequestSpecification spec01;
+	protected RequestSpecification spec02;
+	protected RequestSpecification spec03;
 	
 	@Before
 	public void setUp01() {		
 		spec01 = new RequestSpecBuilder().
 				             setBaseUri("https://restful-booker.herokuapp.com").
+				             build();		
+	}
+	
+	@Before
+	public void setUp02() {		
+		spec02 = new RequestSpecBuilder().
+				             setBaseUri("http://dummy.restapiexample.com/api/v1/employees").
+				             build();		
+	}
+	
+	@Before
+	public void setUp03() {		
+		spec03 = new RequestSpecBuilder().
+				             setBaseUri("https://jsonplaceholder.typicode.com/todos").
 				             build();		
 	}
 	
