85b74335606df143778249b82745e79d9d8b4468
apple <apple@apples-imac-6.attlocal.net>
2020-08-23 12:58:19 -0400

    Pojo

--- src/test/java/techproedturkish01/techproedturkish01api/GetRequest12.java
+++ src/test/java/techproedturkish01/techproedturkish01api/GetRequest12.java
@@ -30,7 +30,7 @@ public class GetRequest12 extends TestBase {
 		
         response.prettyPrint();
         
-        //By using GSON
+        //How to convert Json into Java Object by using GSON
         
         List<Map<String, Object>> listOfMapsByGson = response.as(ArrayList.class);
         
@@ -47,38 +47,6 @@ public class GetRequest12 extends TestBase {
         
         //Sondan bir onceki elemanin title'inin 'numquam repellendus a magnam' oldugunu verify ediniz
         softAssert.assertEquals(listOfMapsByGson.get(listOfMapsByGson.size()-2).get("title"), "numquam repellendus a magnam");
-        
-        //How to convert Java Object into Json by using GSON
-        Gson gsonObj = new Gson();
-        System.out.println(gsonObj.toJson(listOfMapsByGson));
-		
-        
-        //By Using Jackson ObjectMapper
-        
-		List<Map<String, Object>> listOfMapsByJackson = JsonUtil.convertJsonToJava(response.asString(), ArrayList.class);
-		System.out.println("Convertod to Java Object by Jackson: " + listOfMapsByJackson);
-		
-		//200 tane id oldugunu 'verify' ediniz.
-        softAssert.assertTrue(listOfMapsByJackson.size()==200,"Id sayisi istenen gibi degil");
-             
-        //121. elemanin completed degerinin true oldugunu verify ediniz
-        softAssert.assertEquals(listOfMapsByJackson.get(120).get("completed"), true, "Istenen gibi degil");
-        
-        //Sondan bir onceki elemanin title'inin 'numquam repellendus a magnam' oldugunu verify ediniz
-        softAssert.assertEquals(listOfMapsByJackson.get(listOfMapsByJackson.size()-2).get("title"), "numquam repellendus a magnam");
-		softAssert.assertAll();
-		
-		//How to convert Java Object into Json by using Jackson
-		String jsonString = JsonUtil.convertJavaToJson(listOfMapsByJackson);
-		System.out.println("Java Object converted into Json" + jsonString);
-		
-		
-		
-		
-		
-		
-		
-		
-    
+
 	}
 }
